13aa4f05bba71d8996e251fed8851594fc6971b6
Stephen Oba <obastepheno@gmail.com>
2022-05-18 00:01:42 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql:///flask-example'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/example'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+
+db.create_all()
+
+
+@app.route('/')
+def index():
+    person = Person.query.first()
+    return "Hello, " + person.name
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0')
+
